b14d9d4b1746df81da98177f58827c5762d48b7d
Max Partenfelder <max.partenfelder@wilde-it.com>
2021-10-10 19:35:20 +0200

    added authentication and authorization to controllers

--- src/util.ts
+++ src/util.ts
@@ -31,6 +31,61 @@ if (process.env.NODE_ENV !== 'production') {
     }));
 }
 
-export const contextBuilder = async ({ req }) => {
+export type Group = {
+    key: String,
+    name: String
+}
+
+export type User =  {
+    id: Number | String,
+    firstName: String,
+    lastName: String,
+    username: String,
+    email: String,
+    groups: Group[]
+}
+
+export type ContextType = {
+    auth: Boolean,
+    user: User | null
+}
+
+export const contextBuilder = async ({ req }): Promise<ContextType> => {
     // build your context based on your request here
+    // when authentication is needed, call your user api here and check if the request is authenticated
+    // then build your user object and place it in the context variable
+    // don't forget to set `auth` to `true` if the request is authenticated
+    let context: ContextType = {auth: false, user: null}
+    return context;
+}
+
+export function authedUserInGroups(context, groups) {
+    if(typeof(context.user) === 'undefined' || context.user === null ||
+        typeof(context.user.groups) === 'undefined' || context.user.groups === null || context.auth === false) {
+        return false;
+    }
+
+    for(let groupKey of groups) {
+        for(let userGroup of context.user.groups) {
+            if(groupKey === userGroup.key) {
+                return true;
+            }
+        }
+    }
+
+    // Always return true for sudo
+    for(let userGroup of context.user.groups) {
+        if("sudo" === userGroup.key) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+export function authedUserHasId(context, id) {
+    if(typeof(context.user) === 'undefined' || context.user === null || context.auth === false) {
+        return false;
+    }
+    return context.user.id === id;
 }
